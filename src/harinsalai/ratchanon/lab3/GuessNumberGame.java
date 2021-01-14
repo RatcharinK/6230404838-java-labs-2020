c25a541c337d54fb7785e0f2efb27308ee9a1e47
ratchanon <ratchanon.ha@kkumail.com>
2021-01-15 01:07:56 +0700

    Update Lab3 test1

--- src/harinsalai/ratchanon/lab3/GuessNumberGame.java
+++ src/harinsalai/ratchanon/lab3/GuessNumberGame.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 
 public class GuessNumberGame {
     public static void main(String[] args) {
-        System.out.println("Please enter a guess (1-10): ");
-
         //Generate random number
         Random number = new Random();
         int upperbound = 10;
@@ -15,8 +13,24 @@ public class GuessNumberGame {
         //Create Scanner
         Scanner scan = new Scanner(System.in);
 
-        //for (int i == 0 ; i < 3 ; i++;) {
+        int remainingtries = 3; //Create number of remaining tries variable
 
-
+        //Loop
+        for (int i = 0; i < 5 ; i++) {
+            System.out.println("Please enter a guess (1-10):");
+            int guess = scan.nextInt();
+            remainingtries--;
+            if (guess == randomnumber) {
+                System.out.println("Congratulations ! That's correct");
+                System.exit(0);
+            } else if (guess > randomnumber) {
+                System.out.println("Please type a lower number! Number of remaining tries:" + remainingtries);
+            } else if (guess < randomnumber) {
+                System.out.println("Please type a higher number! Number of remaining tries:" + remainingtries);
+            }
+            if (remainingtries == 0) {
+                System.exit(0);
+            }
+        }
     }
 }
