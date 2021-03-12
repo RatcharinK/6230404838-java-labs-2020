3c0d7106f3973d680243d0bf32448533dd9b980d
ratchanon <ratchanon.ha@kkumail.com>
2021-03-12 10:14:03 +0700

    Update TestGame

--- src/harinsalai/ratchanon/lab7/TestGameUsingPolymorphism.java
+++ src/harinsalai/ratchanon/lab7/TestGameUsingPolymorphism.java
@@ -1,4 +1,24 @@
 package harinsalai.ratchanon.lab7;
 
-public class TestGameUsingPolymorphism {
+import harinsalai.ratchanon.lab6.DiceGame;
+import harinsalai.ratchanon.lab6.Game;
+import harinsalai.ratchanon.lab6.GuessNumberGame;
+
+import java.util.ArrayList;
+
+public abstract class TestGameUsingPolymorphism extends Game {
+    static ArrayList<Game> game =
+            new ArrayList<Game>(0);
+
+    public static void main(String[] args) {
+        game.add(new DiceGame());
+        game.add(new DiceGame("h"));
+        game.add(new GuessNumberGame());
+        game.add(new DiceGame("l"));
+        game.add(new GuessNumberGame(1, 6, 1));
+        for (Game game : game) {
+            System.out.println(game);
+            game.playGame();
+        }
+    }
 }
